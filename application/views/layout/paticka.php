4acc14f3f4f9f5f824a71022cebe07804bc76302
JakubKatrnak <katrnakjakub@gmail.com>
2020-10-15 19:42:37 +0200

    main

--- application/views/layout/paticka.php
+++ application/views/layout/paticka.php
@@ -1,3 +1,7 @@
+        
+        <a href="https://oauh-my.sharepoint.com/:b:/g/personal/katrnak_jakub_oauh_cz/EcNf77JJ_INFjQi1MQKZaN0BQ87iXxaNO8XJpb7FChUzgQ?e=Q5oH2T"><img id="dokumentace" src="obrazky/icon_book.png"></a>
+        <a style="color: #073642" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">@</a>
+        
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
